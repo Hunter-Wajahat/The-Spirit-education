809278481e855b9f2ffbaf379d01b2e30e9d903c
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-08-19 02:12:17 +0500

    added a handler to authanticate the user for bieng author

---
+++ Back-end-server/controllers/authorHandler.js
@@ -0,0 +1,28 @@
+const Author = require('../models/authorSchema')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = process.env.JWT_SECRET;
+async function handleAuthor(req, res) {
+    const { username, password } = req.body;
+    try {
+
+        if (password == process.env.author_PASS) {
+            const author = new Author({ username, password })
+            await author.save()
+
+            const payload = {
+                user: username,
+                pass: password
+            }
+            const token = jwt.sign(payload, JWT_SECRET)
+
+            res.cookie("authorized", token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: "none"
+            }).json({ success: true, redirectUrl: process.env.frontend_server });
+        }
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+}
