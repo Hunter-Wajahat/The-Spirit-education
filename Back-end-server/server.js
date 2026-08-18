e1a9b2419d846536482ec464917b0e8afb0fd13f
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-08-19 02:13:11 +0500

    addes a cors middlware for frontend

--- Back-end-server/server.js
+++ Back-end-server/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express()
 const port = 3000
-const connectDB = require('./models/db')
+const connectDB = require('./models/db');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
 
 connectDB()
+
+app.use(cors({
+  origin: process.env.frontend_server, // My frontend
+  credentials: true               // allow sending cookies
+}));
+
+app.use(cookieParser())
+
 app.get('/', (req, res) => {
   res.json('we are listening...')
 })
