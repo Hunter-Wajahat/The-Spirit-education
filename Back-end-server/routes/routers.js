f869e177ab60177d462e9e1b4c40bc89a9857df4
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-08-19 11:58:35 +0500

    added post route for auth

--- Back-end-server/routes/routers.js
+++ Back-end-server/routes/routers.js
@@ -1,2 +1,11 @@
-const router = express.router();
+const express = require('express');
+const handleAuthor = require("../controllers/authorHandler");
 
+const router = express.Router();
+
+//POST requests
+router.post('/signin', handleAuthor)
+
+//GET requests
+router.get('/my_dashboard')
+module.exports = router;
