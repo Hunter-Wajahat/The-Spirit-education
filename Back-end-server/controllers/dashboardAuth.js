e936e50018a9b8ab3b10954452a5646d0b95033c
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-08-20 02:19:01 +0500

    added handler for dashboard to authanticate the admin

---
+++ Back-end-server/controllers/dashboardAuth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+function DashboardAuth(req, res) {
+    try {
+        const myToken = req.cookies.authorized;
+        if (!myToken) {
+            res.status(401).json({ admin: false, message: "unauthorized" })
+        }
+        const decoded = jwt.verify(myToken, process.env.JWT_SECRET)
+        console.log("the decoded token: ", decoded)
+        res.json({ username: decoded.user, admin: decoded.admin })
+
+    } catch (err) {
+        res.json({ message: `there is an error: ${err}` })
+    }
+}
+
+module.exports = DashboardAuth;
