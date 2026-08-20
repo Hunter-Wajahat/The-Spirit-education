c6105c237f8047b1f85173a9dd78ef62a837e409
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-08-20 11:50:40 +0500

    modified the BlogSchema

--- Back-end-server/models/blogSchema.js
+++ Back-end-server/models/blogSchema.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
+const mongoose = require('mongoose');
 
-const blogSchema = new Schema({
+const blogSchema = new mongoose.Schema({
     tittle: String,
     author: String,
     body: String,
     date:{type: Date, default: Date.now},
-    catagory: String
+    catagory: String,
+    publish: Boolean,
 });
 
 const Blog = mongoose.model('Blog', blogSchema);
