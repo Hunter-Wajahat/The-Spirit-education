8879ecc14fe83af375b5d8b946945289d20979f4
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-08-20 11:55:56 +0500

    added dashboard route and post_blog

--- Back-end-server/routes/routers.js
+++ Back-end-server/routes/routers.js
@@ -2,11 +2,13 @@ const express = require('express');
 const handleAuthor = require("../controllers/authorHandler");
 const DashboardAuth = require('../controllers/dashboardAuth');
 const authMiddleware = require('../middlewares/authanticationMiddlware');
+const postBloag = require('../controllers/postBlog');
 
 const router = express.Router();
 
 //POST requests
 router.post('/signin', handleAuthor)
+router.post('/post_blog', authMiddleware, postBloag)
 
 //GET requests
 router.get('/my_dashboard',authMiddleware,  DashboardAuth)
