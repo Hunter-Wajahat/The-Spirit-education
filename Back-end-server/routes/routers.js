26395e782f8350c5786107aa6cb147b28bafc329
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-08-22 11:25:54 +0500

    added a middlware

--- Back-end-server/routes/routers.js
+++ Back-end-server/routes/routers.js
@@ -2,13 +2,14 @@ const express = require('express');
 const handleAuthor = require("../controllers/authorHandler");
 const DashboardAuth = require('../controllers/dashboardAuth');
 const authMiddleware = require('../middlewares/authanticationMiddlware');
-const postBloag = require('../controllers/postBlog');
+const postBlog = require('../controllers/postBlog');
+const upload = require('../middlewares/upload')
 
 const router = express.Router();
 
 //POST requests
 router.post('/signin', handleAuthor)
-router.post('/post_blog', authMiddleware, postBloag)
+router.post('/post_blog', upload.single("blogimg"),authMiddleware, postBlog)
 
 //GET requests
 router.get('/my_dashboard',authMiddleware,  DashboardAuth)
