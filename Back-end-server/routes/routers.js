91a710255db4b10313f57eb5441486b358ba0dc9
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-08-23 12:03:14 +0500

    added a route to to give blog data to user by ID

--- Back-end-server/routes/routers.js
+++ Back-end-server/routes/routers.js
@@ -3,7 +3,9 @@ const handleAuthor = require("../controllers/authorHandler");
 const DashboardAuth = require('../controllers/dashboardAuth');
 const authMiddleware = require('../middlewares/authanticationMiddlware');
 const postBlog = require('../controllers/postBlog');
-const upload = require('../middlewares/upload')
+const upload = require('../middlewares/upload');
+const publishBlogs = require('../controllers/publishBlogs');
+const getPublicblogById = require('../controllers/publicBlogByID');
 
 const router = express.Router();
 
@@ -13,4 +15,6 @@ router.post('/post_blog', upload.single("blogimg"),authMiddleware, postBlog)
 
 //GET requests
 router.get('/my_dashboard',authMiddleware,  DashboardAuth)
+router.get('/public_blogs', publishBlogs)
+router.get('/single_blog', getPublicblogById)
 module.exports = router;
