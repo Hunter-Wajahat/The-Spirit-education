6a471cc0eb62ba22c44d62ee4728ad787115121c
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-08-26 02:25:12 +0500

    send the blog by ID

--- Back-end-server/controllers/publicBlogByID.js
+++ Back-end-server/controllers/publicBlogByID.js
@@ -1,12 +1,14 @@
 const Blog = require('../models/blogSchema')
 async function getPublicblogById(req, res) {
     try {
-        const {blogId} = req.body;
+        console.log(req.query)
+        const {blogId} = req.query;
         const blogData = await Blog.findById(blogId)
         console.log(blogData)
         res.json(blogData)
     } catch (err) {
         res.json({message: "there is an error while fetching blog:", error: err})
+        console.log(err)
     }
 }
 module.exports = getPublicblogById;
