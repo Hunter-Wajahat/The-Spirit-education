44c139a492b6e1336bb698fdacd9252b474f6ae0
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-09-09 19:56:47 +0500

    modified the handler to update the blog post

--- Back-end-server/controllers/postBlog.js
+++ Back-end-server/controllers/postBlog.js
@@ -2,8 +2,23 @@ const Blog = require('../models/blogSchema')
 async function postBlog(req, res) {
     console.log(req.body)
     try {
-        const { author, body, catagory, title, publish, } = req.body;
+        const { _id, author, body, catagory, title, publish, } = req.body;
         console.log(req.body)
+
+        if (_id) {
+            const blog = await Blog.findByIdAndUpdate(
+                _id,
+                { author, body, catagory, tittle: title, publish },
+                { new: true, runValidators: true }
+            )
+
+            if (!blog) {
+                return res.status(404).json({ message: "blog not found" })
+            }
+
+            return res.status(200).json({ message: "blog is updated", blog })
+        }
+
         const blog = new Blog({
             author,
             body,
