61221e107aa5d5fd9fb0f5848544ed7ea4038b95
Hunter-Wajahat <1hunter2wajahat@gmail.com>
2026-09-16 20:58:15 +0500

    modified

--- Back-end-server/controllers/giveAllBlogs.js
+++
@@ -1,7 +0,0 @@
-const Blog = require('../models/blogSchema')
-async function giveAllBlogsToAuthor(req, res) {
-    const allBlogs = await Blog.find();
-    res.json(allBlogs)
-}
-
-module.exports = giveAllBlogsToAuthor;
--- Back-end-server/controllers/publishBlogs.js
+++
@@ -1,6 +0,0 @@
-const Blog = require('../models/blogSchema')
-async function publishBlogs(req, res) {
-    const blogs = await Blog.find({ publish: true })
-    res.json(blogs)
-}
-module.exports = publishBlogs;
